e1b65de797a70980fc7cc26bf6b65f24b82c9349
Pako-12317 <patrykm10@nycstudents.net>
2025-02-07 13:58:22 +0000

    ,commit

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -196,7 +196,7 @@ public class Main {
     public static ArrayList<Integer> notAlone(ArrayList<Integer> list, int val){
         for (int i = 0; i < list.size(); i++){
             if(val == list.get(i) && i==0){
-                list.set(0, list.get(i + 1));
+                list.set(0, list.get(i+1));
             } else if (val == list.get(i)){
                 if (i > 0 && i < list.size() - 1) {
                     if (list.get(i - 1) != val && list.get(i + 1) != val) {
